b489aa22ee59fdbf32fe3dca168796c47f20b2d9
Phillip Reichelt <preichelt@ou.edu>
2014-11-09 21:48:47 -0600

    Adds friendly-id to recipe model.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,16 @@
 class Recipe < ActiveRecord::Base
+  extend FriendlyId
   strip_attributes
 
   validates :name, presence: true
+
+  friendly_id :slug_candidates, use: [:slugged, :finders]
+
+  def slug_candidates
+    [
+      :name,
+      [:name, :source],
+      [:name, :image]
+    ]
+  end
 end
